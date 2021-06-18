39fd779b53ef8db59c4d89963eaf6a08ac8ad320
Vernaaa <79572940+Vernaaa@users.noreply.github.com>
2021-06-19 01:07:09 +0800

    Delete asset/css directory

--- asset/css/js/addheart.js
+++
@@ -1,3 +0,0 @@
-function addheart(){
-    alert("已成功加入收藏清單!")
-}
--- asset/css/js/open_comment.js
+++
@@ -1,6 +0,0 @@
-function openwin() {
-
-    window.open("comment.html", "newwindow",
-      "height=600, width=700, toolbar=no, menubar=no, scrollbars=no, resizable=no, location=no, status=no")
-
-}
